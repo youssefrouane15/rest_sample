c86b5104cd349dae9d2f1d80c62e4a51870ba214
Hamza <sondes.hamza@gmail.com>
2019-03-14 12:46:35 +0100

    First commit for the frontend angular for the api restful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers :[]
 })
 export class AppComponent {
   title = 'frontend-rest';
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,73 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ToastrModule } from 'ngx-toastr';
+import { appRoutes } from './routerConfig';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material'
+import { DeleteClientComponent } from './delete-client/delete-client.component';
+import { ListClientComponent } from './list-client/list-client.component';
+import { AddClientComponent } from './add-client/add-client.component';
+import { EditClientComponent } from './edit-client/edit-client.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormControl } from "@angular/forms";
+
+import {
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatInputModule,
+  MatIconModule,
+  MatMenuModule
+} from '@angular/material';
+import { ReadClientComponent } from './read-client/read-client.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    LoginComponent,
+    DeleteClientComponent,
+    ListClientComponent,
+    AddClientComponent,
+    EditClientComponent,
+    PageNotFoundComponent,
+    ReadClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule.forRoot(appRoutes),
+    HttpModule, BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    ReactiveFormsModule, FormsModule,
+    MatMenuModule, MatButtonModule, MatIconModule,
+    MatTableModule, MatPaginatorModule, FormsModule,
+    HttpClientModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule
+
+
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+}
