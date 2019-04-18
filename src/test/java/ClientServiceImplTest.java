6c291d5b8016db95f6dbcbf1d3f90eabbfec35e1
Hamza <sondes.hamza@gmail.com>
2019-04-18 17:14:32 +0200

    reset change

--- src/test/java/ClientServiceImplTest.java
+++ src/test/java/ClientServiceImplTest.java
@@ -35,15 +35,5 @@ ClientServiceImpl clientServiceImpl =new ClientServiceImpl(clientRepository);
     assertEquals("Societe generale", clientServiceImpl.findAll().get(0).getFirstName());
     assertEquals("ALMA Group", clientServiceImpl.findAll().get(1).getFirstName());
 }
-    @Test
-    public void findClientById() throws  Exception{
-        List<Client> clients = new ArrayList<>();
-        Client client1 = new Client("A1", "Societe generale", "SG", "sondes.hamza@gmail.com", "064455676", "10/10/1990");
-        Client  client2 =new Client("A2", "ALMA Group", "AG", "sondes.hamza@gmail.com", "076565764", "10/10/2000");
-        clients.add(client1);
-        clients.add(client2);
-        when(clientRepository.findById(1L).get()).thenReturn(client1);
-        assertEquals(client1, clientServiceImpl.findById(1L).get());
 
-    }
 }
